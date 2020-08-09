974c180ed47b98403396e45ee759cd69abc912ff
Nabarun Pal <pal.nabarun95@gmail.com>
2020-08-09 21:56:50 +0530

    [milestone:2] Add support for the user to specify their own short URLs
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import string
+from typing import Union
 
 from flask import Flask, request, redirect
 
@@ -8,29 +9,54 @@ logger = logging.getLogger()
 
 app = Flask(__name__)
 
+# {"short_url1": "url1", "short_url2": "url2"}
 url_map = {}
 
 CHARACTER_CHOICES = string.ascii_letters+string.digits
 SHORT_URL_LENGTH = 5
 
 
-def shorten_url(url: str) -> str:
+def get_random_string(choices: Union[list, tuple, str], length: int) -> str:
+    return "".join(random.choices(choices, k=length))
+
+
+def shorten_url(url: str, short_url: str="") -> str:
     global url_map
 
+    og_short_url = short_url
+
     # very smart collision checker in 3 lines
-    random_id = "".join(random.choices(CHARACTER_CHOICES, k=SHORT_URL_LENGTH))
+    short_url = og_short_url + get_random_string(CHARACTER_CHOICES, SHORT_URL_LENGTH)
     i = 0
-    while url_map.get(random_id, ""):
+    while url_map.get(short_url, ""):
         i += 1
         logger.warn(f"---------- Detected collision {i} times")
-        random_id = "".join(random.choices(CHARACTER_CHOICES, k=SHORT_URL_LENGTH))
+        short_url = og_short_url + get_random_string(CHARACTER_CHOICES, SHORT_URL_LENGTH)
 
-    return random_id
+    return short_url
 
 
 def lengthen_url(short_url: str) -> str:
     return url_map.get(short_url, "")
 
+
+class URLMapping:
+    def __init__(self, url: str, short_url: str, strict: bool):
+        self.url = url
+        self.short_url = short_url
+        self.strict = strict
+
+    @staticmethod
+    def create(url: str, short_url: str="", strict: bool=True):
+        if strict and short_url and short_url in url_map:
+            raise KeyError(f"short_url {short_url} already assigned")
+
+        if not strict and short_url and short_url in url_map:
+            short_url = shorten_url(url, short_url)
+        else:
+            short_url = shorten_url(url) if not short_url else short_url
+
+        return URLMapping(url=url, short_url=short_url, strict=strict)
 
 @app.route("/api", methods=["POST"])
 def create_short_url():
@@ -44,10 +70,17 @@ def create_short_url():
         logger.error(message)
         return {"error": message}, 400
 
-    short_url = shorten_url(url)
-    url_map[short_url] = url
+    strict = payload.get("strict", True)
+    short_url = payload.get("short_url", "")
 
-    return {"url": url, "short_url": short_url}
+    try:
+        url_mapping = URLMapping.create(url, short_url, strict)
+    except KeyError as e:
+        return {"error": f"{e}. Please use strict=false for getting a random id appended"}, 409
+
+    url_map[url_mapping.short_url] = url_mapping.url
+
+    return {"url": url_mapping.url, "short_url": url_mapping.short_url}
 
 
 @app.route("/<short_url>")
